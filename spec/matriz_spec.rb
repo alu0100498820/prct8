ced3fa1c7a9be60c8c06db7efca29ce2fe6cf57d
Borja Barrera Villagrasa <alu0100498820@ull.edu.es>
2013-11-05 18:23:22 +0000

    Se incluye la primera espectativa

---
+++ spec/matriz_spec.rb
@@ -0,0 +1,10 @@
+require "matriz.rb"
+
+describe Matriz do
+        before :each do
+                @m1 = Matriz.new(2,2,[[1,9],[9,4]])
+                @m2 = Matriz.new(2,2,[[5,4],[4,8]])
+              
+        end
+end
+
