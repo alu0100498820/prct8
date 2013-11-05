e1971ca4dbf75e6b18cda7f62bd05cdc43623431
Borja Barrera Villagrasa <alu0100498820@ull.edu.es>
2013-11-05 18:54:28 +0000

    comprobacion filas columnas 2x2

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -2,9 +2,17 @@ require "matriz.rb"
 
 describe Matriz do
         before :each do
-                @m1 = Matriz.new(2,2,[[1,9],[9,4]])
-                @m2 = Matriz.new(2,2,[[5,4],[4,8]])
+                @m1 = Matriz.new([[1,9],[9,4]])
+                @m2 = Matriz.new([[5,4],[4,8]])
               
+        end
+	describe "Comprobacion: " do
+               it "Filas matriz 2x2" do
+                       @m1.fil.should eq(2)
+               end
+               it "Columnas matriz 2x2" do
+                       @m1.colum.should eq(2)
+               end
         end
 end
 
