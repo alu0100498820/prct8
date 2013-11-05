951c2eb0d73bc0b6b4d802f7bcb337604e76ae0b
Ayose Castillo Barroso <alu0100600810@ull.edu.es>
2013-11-05 19:08:49 +0000

    Se cumple la espectativa para la suma

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -7,4 +7,18 @@ class Matriz
                 @colum = colum
                 @elem = Array.new(elem)
         end
+
+
+	def +(other)
+                
+                elem = Array.new
+                @fil.times do |i|
+                        elem_fil = Array.new
+                        @colum.times do |j|
+                                elem_fil << @elem[i][j] + other.elem[i][j]
+                        end
+                        elem << elem_fil
+                end
+                Matriz.new(@fil, @colum,elem)
+        end
 end
